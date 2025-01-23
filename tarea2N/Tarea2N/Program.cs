deb49cdeef69d5afaa9c4a04cd17ceb21d36867d
Mauricio2005-Hub <carlosmauriciochalemendez@gmail.com>
2025-01-23 11:34:08 -0600

    Add files via upload

---
+++ tarea2N/Tarea2N/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número para determinar si este es un numero primo:");
+        int numero = int.Parse(Console.ReadLine());
+            if (EsPrimo(numero))
+            {
+                Console.WriteLine($"El número {numero} es primo.");
+            }
+        else
+            {
+                Console.WriteLine($"El número {numero} no es primo.");
+            }
+        }
+        static bool EsPrimo(int n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0) 
+                    return false;
+            }
+            return true;
+        }
+    }
